f5ceb98c57b4d111fe1dd6a8a610c3205789c36e
Roberto16121 <lazarroberto121@gmail.com>
2023-03-04 20:22:00 +0200

    Better save functionality
    Added a better way to save files and fix some bugs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,86 +1,100 @@
 ﻿using System;
 using System.IO;
-using System.Text;
-using System.Transactions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
-using TextEditor.MVVM.View;
+using System.Windows.Forms;
 
 namespace TextEditor
 {
     public partial class MainWindow : Window
     {
-        public string path;
-        public bool saved = true;
-        public string name = null;
+        public string path = "";     //Stores the path to the MyDocuments
+        public bool saved = true;   //stores the state of the file
+        public string name = "";    //name of the file
         public MainWindow()
         {
             InitializeComponent();
-            path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            path = path +@"\TxtEditor\";
-            System.IO.Directory.CreateDirectory(path);
-            
         }
-        private void SaveToFolder(object sender)
+        private void SaveToFolder()
         {
-            if(name == null)
-            {
-                Test1 test1= new Test1();
-                test1.Show();
-                return;
-            }
-            string txtName = name + @".txt";
-            using (StreamWriter sw = File.CreateText(path + txtName))
-                sw.Write(StringFromRichTextBox(TextB));//get all text from rich text
+            string txtName = path + @".txt";    //ads .txt to the file name
+            using StreamWriter sw = File.CreateText(txtName); // Creates the file in directory
+            sw.Write(StringFromRichTextBox(TextB));     //Get's all text from rich text box
         }
-        private void Save_Click(object sender, RoutedEventArgs e)
+        private void Save_Click(object sender, RoutedEventArgs e)   //Click on save button
         {
+            SaveFileDialog saveFileDialog = new();
+            saveFileDialog.ShowDialog();
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.AddExtension = true;
 
-            SaveToFolder(sender); 
-        }
-        public void GetName(string name)
-        {
-            this.name = name;
-            SaveToFolder(this);
+            path = saveFileDialog.FileName;
+            SaveToFolder();
         }
 
-        public string StringFromRichTextBox(RichTextBox rtb)
+        private static string StringFromRichTextBox(System.Windows.Controls.RichTextBox rtb)    //Get's the text from rich text box
         {
-            TextRange textRange = new TextRange(
-                // TextPointer to the start of content in the RichTextBox.
-                rtb.Document.ContentStart,
-                // TextPointer to the end of content in the RichTextBox.
-                rtb.Document.ContentEnd
-            );
-
-            // The Text property on a TextRange object returns a string
-            // representing the plain text content of the TextRange.
-            return textRange.Text;
+            string richText = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd).Text;   
+            return richText;
         }
 
         private void New_Click(object sender, RoutedEventArgs e)
         {
-
+            
+            /* LOAD
+            using (var dialog = new FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                path = dialog.SelectedPath;
+            }*/
         }
-
+        private bool maximized = false;     //var for storing the state of the window
         private void WindowRes_Click(object sender, RoutedEventArgs e)
         {
             Window window = Window.GetWindow(this);//make it fullscreen
-            window.Width = 1920;
-            window.Height = 1080;
-            
+            if (!maximized)
+            {
+                window.WindowState = WindowState.Maximized;
+                maximized = true;
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+                maximized = false;
+            }
         }
 
-        private void Close_Click(object sender, RoutedEventArgs e)
+        private void Close_Click(object sender, RoutedEventArgs e)      //Click on close button
         {
-            if (saved)
+            if (saved)      //if saved then close
                 this.Close();
+            else SaveToFolder();    //else save
         }
 
-        private void TextB_TextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        private void TextB_TextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)    //if the user writes in the text box
         {
-            saved = false;
+            saved = false;      //this means that you can't save
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        //Functionality for moving the window
+        private void Header_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitHeader(sender as DockPanel);
+        }
+        private void InitHeader(DockPanel header)
+        {
+
+            header.MouseLeftButtonDown += (s, e) =>
+            {
+                DragMove();
+            };
         }
     }
 }
